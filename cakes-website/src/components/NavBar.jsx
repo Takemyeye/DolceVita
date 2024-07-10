0d95555f39fe0d349aff88e3027b56cab14951e0
Takemyeye <mellovan2005@gmail.com>
2024-07-10 14:30:01 +0200

    Start
    header start

---
+++ cakes-website/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './styles/navBar.css';
+
+const NavBar = () => {
+    return(
+        <header>
+            <div className="title">
+
+            </div>
+            <div className="nav-container">
+                <span>Catalog</span>
+                <span>About</span>
+                <span></span>
+                <span>Contact Us</span>
+            </div>
+        </header>            
+    );
+};
+
+export default NavBar;
